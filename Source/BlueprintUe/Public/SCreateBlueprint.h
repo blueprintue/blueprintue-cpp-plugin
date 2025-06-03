b1413ede42e073e2d9798d2e50f90f5853359055
Sébastien Rancoud <rancoud@users.noreply.github.com>
2025-06-03 20:02:52 +0200

    feat: add unreal engine version 5.6

--- Source/BlueprintUe/Public/SCreateBlueprint.h
+++ Source/BlueprintUe/Public/SCreateBlueprint.h
@@ -104,7 +104,7 @@ private:
 		UEVersion = VERSION_STRINGIFY(ENGINE_MAJOR_VERSION) VERSION_TEXT(".") VERSION_STRINGIFY(ENGINE_MINOR_VERSION);
 		UEVersionOptions.Empty();
 		int currentUEVersionIndex = 0;
-		for (int i = 5; i >= 0; --i)
+		for (int i = 6; i >= 0; --i)
 		{
 			FString tmpUEVersion = FString::Printf(TEXT("5.%d"), i);
 			UEVersionOptions.Add(MakeShareable(new FString(tmpUEVersion)));
